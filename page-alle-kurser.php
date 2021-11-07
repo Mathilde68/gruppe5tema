2899dda9be3e111549f9783aa313c4d620706eb0
Mathilde <mathilde68n8@stud.kea.dk>
2021-11-07 12:00:38 +0100

    ungdomsuddanellser og kurser

--- page-alle-kurser.php
+++ page-alle-kurser.php
@@ -13,13 +13,25 @@ get_header();
 	<section id="section" class="content-area">
 
 		<main id="main" class="site-main">
+			<section id="oversigt"></section>
 		</main><!-- #main -->
 		
+		<template>
+        <article class="kurset">
+         <h3 class="navn"></h3>
+		 <img src="" alt="" class="billede">
+		 <p class="beskrivelse"></p>
+        </article>
+    </template>
+
 <script>let kurser;
       
 	  //url til restdb 
 	  const url = "https://xn--mflingo-q1a.dk/kea/ungdomsbyen/wp-json/wp/v2/kursus?per_page=100";
-	 
+
+	 //const for destination og template
+	  const destination = document.querySelector("#oversigt");
+            let template = document.querySelector("template");
 
 	  // asynkron function som afventer og indhenter json data fra restdb
 	  async function hentData() {
@@ -30,6 +42,22 @@ get_header();
 
 	  function visKurser(){
 		  console.log(kurser);
+
+		  kurser.forEach(kursus => {
+               
+        
+                    const klon = template.cloneNode(true).content;
+
+                    klon.querySelector(".navn").textContent = kursus.title.rendered;
+                    klon.querySelector("img").src = kursus.billede.guid;
+                    klon.querySelector(".beskrivelse").textContent = kursus.kort_beskrivelse;
+
+                    klon.querySelector(".kurset").addEventListener("click", () => visDetaljer(kursus));
+                
+
+
+                    destination.appendChild(klon);
+				});
 	  }
 
 	  hentData();
