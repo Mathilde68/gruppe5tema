69ca7677bf84a63901ddbebc1c2775a9be78053d
Mathilde <mathilde68n8@stud.kea.dk>
2021-11-07 12:16:01 +0100

    til kommuner

---
+++ page-til-kommunen.php
@@ -0,0 +1,70 @@
+<?php
+
+
+
+
+
+get_header();
+
+?>
+
+
+
+	<section id="section" class="content-area">
+
+	<main id="main" class="site-main">
+			<section id="oversigt"></section>
+		</main><!-- #main -->
+		
+		<template>
+        <article class="kurset">
+         <h3 class="navn"></h3>
+		 <img src="" alt="" class="billede">
+		 <p class="beskrivelse"></p>
+        </article>
+    </template>
+
+		
+<script>let kurser;
+      
+	  //url til wp restapi db - læg mærke til den her kunindhenter data med kategori 8 (numreringen på til kommunen kategorien)
+	  const url = "https://xn--mflingo-q1a.dk/kea/ungdomsbyen/wp-json/wp/v2/kursus?categories=8";
+	 
+
+	  // asynkron function som afventer og indhenter json data fra restdb
+	  async function hentData() {
+		  const jsonData = await fetch(url);
+		  kurser = await jsonData.json();
+		  visKurser();
+	  }
+
+	  function visKurser(){
+		  console.log(kurser);
+
+		  kurser.forEach(kursus => {
+               
+        
+			   const klon = template.cloneNode(true).content;
+
+			   klon.querySelector(".navn").textContent = kursus.title.rendered;
+			   klon.querySelector("img").src = kursus.billede.guid;
+			   klon.querySelector(".beskrivelse").textContent = kursus.kort_beskrivelse;
+
+			   klon.querySelector(".kurset").addEventListener("click", () => visDetaljer(kursus));
+		   
+
+
+			   destination.appendChild(klon);
+		   });
+	  }
+
+	  hentData();
+
+</script>
+
+	</section><!-- #section -->
+
+
+<?php
+
+get_footer();
