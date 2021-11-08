c5f8a18190889b27928b72e4ac6df56f81a72a6a
KatrineVedel <44161731+KatrineVedel@users.noreply.github.com>
2021-11-08 17:31:57 +0100

    filtrering gerne færdig

--- page-til-undervisere-og-ledere.php
+++ page-til-undervisere-og-ledere.php
@@ -13,32 +13,20 @@ get_header();
 	<section id="section" class="content-area">
 
 	<main id="main" class="site-main">
-	<section class="filter_section">
-            <div id="tema2">
-                <img src="" alt="">
-                <button>Fn's 17 verdensmål</button>
-            </div>
-            <div id="tema3">
-                <img src="" alt="">
-                <button>LGBTQ+ og normer</button>
-            </div>
-            <div id="tema4">
-                <img src="" alt="">
-                <button >Demokrati og Medborgerskab</button>
-            </div>
-</section>
+
+	<h2>Kursus til undervisere og ledere</h2>
+
+	
+	<nav class="filter_section">
+        <button data-kategori= "alle" class="valgt">Alle</button>
+        <button data-kategori= "tema2">FN´s 17 verdensmål</button>
+        <button data-kategori = "tema3">LGBTQ+ og normer</button>
+        <button data-kategori= "tema4">Demokrati og medborgerskab</button>
+    </nav>
 			<section id="oversigt"></section>
 		</main><!-- #main -->
 		
 		<template>
-
-		<nav>
-        <button data-kategori= "alle" class="valgt">Alle</button>
-        <button data-kategori= "forretter">Forretter</button>
-        <button data-kategori = "hovedretter">Hovedretter</button>
-        <button data-kategori= "desserter">Desserter</button>
-        <button data-kategori= "drikkevarer">Drikkevarer</button>
-    </nav>
 
 		<article class="kurset">
 		<h3 class="navn"></h3>
@@ -52,26 +40,26 @@ get_header();
     </template>
 
 		
-<script>let kurser;
+<script>
+
+let kurser;
       
 	  //url til wp restapi db - læg mærke til den her kun indhenter data med kategori 7 (numreringen på til undervisere og ledere kategorien)
 	  const url = "https://xn--mflingo-q1a.dk/kea/ungdomsbyen/wp-json/wp/v2/kursus?categories=7";
 	 
 //filtrer knap, her defineres der filtreringsknapper og laver click event
-
-let kruser;
 let filter = "alle";
 
-const filterKnapper = document.querySelectorAll("button");
+const filterKnapper = document.querySelectorAll(".filter_section button");
 filterKnapper.forEach(knap => knap.addEventListener("click", filtrerKurser));
 hentData();
 
 function filtrerKurser (){
 	filter = this.dataset.kategori;
-	document.querySelector(".")
+	document.querySelector(".valgt").classList.remove.("valgt");
+	this.classList.add("valgt");
+	visKurser();
 }
-
-
 
  	//const for destinationen af indholdet og templaten
 			 const destination = document.querySelector("#oversigt");
@@ -82,6 +70,7 @@ function filtrerKurser (){
 	  async function hentData() {
 		  const jsonData = await fetch(url);
 		  kurser = await jsonData.json();
+		  console.log("kurser" kurser);
 		  visKurser();
 	  }
 
@@ -89,6 +78,7 @@ function filtrerKurser (){
 		  console.log(kurser);
           destination.textContent = "";
 		  kurser.forEach(kursus => {
+			  if(filter == kursus.tema || filter == "alle"){ 
 			   const klon = template.cloneNode(true).content;
 			   klon.querySelector(".navn").textContent = kursus.navn;
                 klon.querySelector("img").src = kursus.billede.guid;
@@ -99,7 +89,7 @@ function filtrerKurser (){
 			   destination.appendChild(klon);
 		   });
 	  }
-
+	}
 	  hentData();
 
 </script>
