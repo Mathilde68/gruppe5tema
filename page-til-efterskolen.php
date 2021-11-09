bbca9c6c8992ad959ff9568fc38584f8c4118afe
KatrineVedel <44161731+KatrineVedel@users.noreply.github.com>
2021-11-10 00:33:12 +0100

    d

--- page-til-efterskolen.php
+++ page-til-efterskolen.php
@@ -16,6 +16,23 @@ get_header();
 		<section id="oversigt"></section>
 		</main><!-- #main -->
 		
+		<?php
+
+// Start the Loop.
+while ( have_posts() ) :
+    the_post();
+
+    get_template_part( 'template-parts/content/content', 'page' );
+
+    // If comments are open or we have at least one comment, load up the comment template.
+    if ( comments_open() || get_comments_number() ) {
+        comments_template();
+    }
+
+endwhile; // End the loop.
+?>
+
+		
 		<template>
 		<article class="kurset">
 		<h3 class="navn"></h3>
